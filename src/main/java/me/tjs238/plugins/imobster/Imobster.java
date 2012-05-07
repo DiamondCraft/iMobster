5621eae98b56be736c84d81dbcc9b68215e7cb71
Tjs238 <tjs238@gmail.com>
2012-05-06 22:30:04 -0400

    Init Commit

---
+++ src/main/java/me/tjs238/plugins/imobster/Imobster.java
@@ -0,0 +1,22 @@
+package me.tjs238.plugins.imobster;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Imobster extends JavaPlugin implements Listener {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    }
+}
+
